94f9f66270f17b8828c66f32e4b1dc9e8ceceff5
iamdharani <dharanichandran77@gmail.com>
2025-11-11 12:53:13 +0530

    Updated footer with SmartCart and dynamic year

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
-    return  <footer className="py-1 bg-dark">
-                <p className="text-center text-white mt-1 ">
-                    JVLcart - 2023-2024, All Rights Reserved
-                </p>
-            </footer>
+    return (
+        <footer className="py-1 bg-dark">
+            <p className="text-center text-white mt-1">
+                SmartCart - {new Date().getFullYear()}, All Rights Reserved
+            </p>
+        </footer>
+    );
 }
